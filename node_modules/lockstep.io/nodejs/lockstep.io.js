44e5ede0ae5977ac6a3825571c13ce5b17c451ee
Zach Bebel <zlb3507@rit.edu>
2016-04-16 12:58:53 -0400

    Created Heroku server for LockstepIO, changed cloud url destination:
    https://github.com/ZachBebel/LockstepIOServer/

--- node_modules/lockstep.io/nodejs/lockstep.io.js
+++ node_modules/lockstep.io/nodejs/lockstep.io.js
@@ -1,73 +1,86 @@
 module.exports = function (io) {
-	var ref = {
-		io: io,
-		commandDelay: 0,
-		sockets: {},
-		randomSeed: parseInt(Math.random() * 10000),
-		onConnection: function (socket) {
-			console.log('connected: ' + socket.id);
+    var ref = {
+        io: io,
+        commandDelay: 0,
+        sockets: {},
+        randomSeed: parseInt(Math.random() * 10000),
+        onConnection: function (socket) {
+            console.log('connected: ' + socket.id);
 
-			// if we have no host make host
-			if (!ref.host) {
-				socket.is_host = true;
-				ref.host = socket.id;
-			}
+            // if we have no host make host
+            if (!ref.host) {
+                socket.is_host = true;
+                ref.host = socket.id;
+            }
 
-			// stash socket reference by id
-			ref.sockets[socket.id] = {socket: socket};
+            // stash socket reference by id
+            ref.sockets[socket.id] = {
+                socket: socket
+            };
 
-			// provide random seed
-			socket.emit('lockstep.io:seed', {randomSeed: ref.randomSeed});
+            // provide random seed
+            socket.emit('lockstep.io:seed', {
+                randomSeed: ref.randomSeed
+            });
 
-			// handle disconnects
-			socket.on('disconnect', function() {
-				console.log('disconnected: ' + socket.id);
+            // handle disconnects
+            socket.on('disconnect', function () {
+                console.log('disconnected: ' + socket.id);
 
-				// if the host disconnected
-				if (ref.host == socket.id) {
-					
-					// delete host reference
-					delete ref['host'];
+                // if the host disconnected
+                if (ref.host == socket.id) {
 
-					// attempt to assign a new host here...
-					///todo
-				}
+                    // delete host reference
+                    delete ref['host'];
 
-				// delete socket reference
-				delete ref.sockets[socket.id];
-			});
+                    // attempt to assign a new host here...
+                    ///todo
+                }
 
-			// handle sync events
-			socket.on('lockstep.io:sync', function (ntp) {
-				if (socket.is_host) {
-					socket.emit('lockstep.io:sync', {t0: ntp.t0, t1: ntp.t0});	
-				} else {
-					socket.emit('lockstep.io:sync', {t0: ntp.t0, t1: ref.sockets[ref.host].sync.localNow});	
-				}
-			});
+                // delete socket reference
+                delete ref.sockets[socket.id];
+            });
 
-			// handle sync complete (ready) events
-			socket.on('lockstep.io:ready', function (ready) {
+            // handle sync events
+            socket.on('lockstep.io:sync', function (ntp) {
+                if (socket.is_host) {
+                    socket.emit('lockstep.io:sync', {
+                        t0: ntp.t0,
+                        t1: ntp.t0
+                    });
+                } else {
+                    socket.emit('lockstep.io:sync', {
+                        t0: ntp.t0,
+                        t1: ref.sockets[ref.host].sync.localNow
+                    });
+                }
+            });
+
+            // handle sync complete (ready) events
+            socket.on('lockstep.io:ready', function (ready) {
 
 
-				console.log(ready);
-				if (ready.roundTrip * 3 > ref.commandDelay) {
-					ref.commandDelay = ready.roundTrip * 3;
-				}
+                //console.log(ready);
+                if (ready.roundTrip * 3 > ref.commandDelay) {
+                    ref.commandDelay = ready.roundTrip * 3;
+                }
 
-				ref.sockets[socket.id].sync = ready;
-				var clients = {};
-				for (var id in ref.sockets) {
-					clients[id] = ref.sockets[id].sync;
-				}
-				var pkg = {commandDelay: ref.commandDelay, clients: clients};
-				ref.io.emit('lockstep.io:ready', pkg);
-			});
-			socket.on('lockstep.io:cmd:issue', function (command) {
-				ref.io.emit('lockstep.io:cmd:issue', command);
-			});
-		}
-	};
-	ref.io.on('connection', ref.onConnection);
-	return ref;
+                ref.sockets[socket.id].sync = ready;
+                var clients = {};
+                for (var id in ref.sockets) {
+                    clients[id] = ref.sockets[id].sync;
+                }
+                var pkg = {
+                    commandDelay: ref.commandDelay,
+                    clients: clients
+                };
+                ref.io.emit('lockstep.io:ready', pkg);
+            });
+            socket.on('lockstep.io:cmd:issue', function (command) {
+                ref.io.emit('lockstep.io:cmd:issue', command);
+            });
+        }
+    };
+    ref.io.on('connection', ref.onConnection);
+    return ref;
 };
